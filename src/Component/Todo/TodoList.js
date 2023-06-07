a8866c79d7135f18dca70909dd096cce842f77bc
gmshimon <simon.rosedale99@gmail.com>
2023-06-07 20:36:55 +0800

    post todo done

--- src/Component/Todo/TodoList.js
+++ src/Component/Todo/TodoList.js
@@ -5,17 +5,34 @@ import { Form } from "react-bootstrap";
 
 const TodoList = () => {
   const nameRef = useRef();
-  const priceRef = useRef();
+  const dateRef = useRef();
   const catRef = useRef();
+  const  timeRef = useRef();
 
   const handleAddTodo = e=>{
     e.preventDefault();
     const name = nameRef.current.value;
-    const price = priceRef.current.value;
-
+    const date = dateRef.current.value;
     const category = catRef.current.value;
+    const time = timeRef.current.value;
 
-    console.log(name, price, category);
+    console.log(name, date, category,time);
+    const todo = {
+      name:name,
+      date:date,
+      category:category,
+      time:time
+    }
+    fetch('http://localhost:5000/api/v1/todos',{
+      method:"POST",
+      headers:{
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(todo)
+    })
+    .then(res=>res.json())
+    .then(data=>console.log(data))
+
   }
   return (
     <div>
@@ -23,7 +40,7 @@ const TodoList = () => {
       <div style={{ marginTop: "80px" }}>
         <h1 className="text-danger text-center">Add Todo</h1>
         <br />
-        <form onSubmit={handleAddTodo} className="w-50 mx-auto ">
+        <form onSubmit={handleAddTodo} className="w-25 mx-auto ">
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Control
               required
@@ -35,9 +52,17 @@ const TodoList = () => {
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Control
               required
-              ref={priceRef}
-              type="text"
+              ref={dateRef}
+              type="date"
               placeholder="Enter Food Price"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Control
+              required
+              ref={timeRef}
+              type="time"
+              placeholder="Time"
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
