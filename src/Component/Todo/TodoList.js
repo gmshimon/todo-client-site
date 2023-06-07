a6a8473909d4379f31b64dce4ce5e72f0f0e4a42
gmshimon <simon.rosedale99@gmail.com>
2023-06-07 14:22:51 +0800

    todo form added

--- src/Component/Todo/TodoList.js
+++ src/Component/Todo/TodoList.js
@@ -1,32 +1,60 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from "react";
 import auth from "../../firebase.init";
-import Head from '../Head/Head';
+import Head from "../Head/Head";
+import { Form } from "react-bootstrap";
 
 const TodoList = () => {
-  const [task, setTask] = useState('');
-  const [tasks, setTasks] = useState([]);
+  const nameRef = useRef();
+  const priceRef = useRef();
+  const catRef = useRef();
 
-  const addTask = () => {
-    setTasks([...tasks, task]);
-    setTask('');
-  };
+  const handleAddTodo = e=>{
+    e.preventDefault();
+    const name = nameRef.current.value;
+    const price = priceRef.current.value;
 
+    const category = catRef.current.value;
+
+    console.log(name, price, category);
+  }
   return (
     <div>
-      <Head/>
-      <div style={{marginTop:'80px'}} >
-      <input
-        type="text"
-        placeholder="Enter a task"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-      />
-      <button onClick={addTask}>Add Task</button>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
-        ))}
-      </ul>
+      <Head />
+      <div style={{ marginTop: "80px" }}>
+        <h1 className="text-danger text-center">Add Todo</h1>
+        <br />
+        <form onSubmit={handleAddTodo} className="w-50 mx-auto ">
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Control
+              required
+              ref={nameRef}
+              type="text"
+              placeholder="Title"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Control
+              required
+              ref={priceRef}
+              type="text"
+              placeholder="Enter Food Price"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <select
+              className="form-select"
+              ref={catRef}
+              aria-label="Default select example"
+            >
+              <option selected>Priority</option>
+              <option defaultValue="BreakFast">High</option>
+              <option defaultValue="Lunch">Medium</option>
+              <option defaultValue="Drinks">Low</option>
+            </select>
+          </Form.Group>
+
+          <input className="btn btn-danger" type="submit" name="" id="" />
+        </form>
       </div>
     </div>
   );
